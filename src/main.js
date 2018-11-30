c47893c6bc03ccafd897458c9698e66fd77bd9f2
PaavoTL <paavo.linkala@hotmail.com>
2018-11-30 12:48:50 +0200

    add getMousePos, draw and move functions
    also add needed event listeners.

--- src/main.js
+++ src/main.js
@@ -15,23 +15,84 @@ class Engine {
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext("2d");
 
+        this.clear.bind(this);
+        this.clear()
+    }
+
+    clear(){
         this.ctx.fillStyle = "#303030";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 
-        this.setup.bind(this);
+    draw(){
+        this.clear();
+        qTree.show(this.ctx);
+
+        this.ctx.strokeStyle = "green";
+        this.ctx.strokeRect(range.x, range.y, range.w, range.h);
+
+        for (let p of points){
+
+            
+            this.ctx.fillStyle = "green"
+            this.ctx.beginPath();
+            this.ctx.arc(p.x, p.y, 3, 0, 2 * Math.PI, false);
+            this.ctx.closePath();
+            this.ctx.fill();
+        }
     }
 
     setup(){
-        console.log("Hello")
-        for(let i = 0; i < 1; i++){
-            let p = new Point(random(this.canvas.width),random(this.canvas.height))
-            qt.insert(p)
+        for (let i = 0; i < 100; i++){
+            let p = new Point(Math.random()*this.canvas.width,Math.random()*this.canvas.height)
+            qTree.insert(p);
         }
     }
 }
 
 let engine = new Engine();
+let boundary = new Rectangle(0, 0, engine.canvas.width, engine.canvas.height);
+let qTree = new QuadTree(boundary, 4);
+let points = [];
 
-let boundary = new Rectangle();
-let qt = new QuadTree(boundary, 4)
+engine.setup();
+console.log(qTree);
 
+let range = new Rectangle(-100, -100, 100, 100)
+
+engine.draw();
+
+
+
+// -|- functions -|-
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+function draw(event){
+    let mousePos = getMousePos(engine.canvas, event);
+    let m = new Point(mousePos.x, mousePos.y);
+    qTree.insert(m);
+    points = qTree.query(range);
+    engine.draw();
+}
+
+function move(event){
+    points = [];
+
+    let mousePos = getMousePos(engine.canvas, event);
+    range.x = mousePos.x - range.w / 2
+    range.y = mousePos.y - range.h / 2
+
+    points = qTree.query(range);
+
+    engine.draw();  
+}
+
+engine.canvas.addEventListener("mousedown", draw);
+engine.canvas.addEventListener("mousemove", move);
