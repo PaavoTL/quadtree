4391204f3c61b1b097c445e3ef9bd260d54bd197
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-03 10:00:06 +0200

    merge

--- src/quadtree.js
+++ src/quadtree.js
@@ -1,7 +1,15 @@
 export class Point {
-    constructor(x,y){
+    constructor(x,y,sx,sy){
         this.x = x;
         this.y = y;
+        this.xspeed = sx || (Math.random() - 0.5) * 100;
+        this.yspeed = sy || (Math.random() - 0.5) * 100;
+        this.check = true;
+    }
+
+    pmove(dt){
+        this.x += this.xspeed * dt;
+        this.y += this.yspeed * dt;
     }
 }
 
@@ -14,10 +22,17 @@ export class Rectangle {
     }
 
     contains(point){
-        return (point.x >= this.x - this.w &&
+        return (point.x >= this.x &&
             point.x <= this.x + this.w &&
-            point.y >= this.y - this.h &&
+            point.y >= this.y &&
             point.y <= this.y + this.h);
+    }
+
+    intersects(range){
+        return !(range.x > this.x + this.w ||
+            range.x + range.w < this.x ||
+            range.y > this.y + this.h ||
+            range.y + range.h < this.y);
     }
 }
 
@@ -35,45 +50,130 @@ export class QuadTree {
         let w = this.boundary.w;
         let h = this.boundary.h;
 
-        let nw = new Rectangle(x-w/2, y-h/2, w/2, h/2);
+        let nw = new Rectangle(x,y,w/2,h/2);
+        let ne = new Rectangle(x+w/2,y,w/2,h/2);
+        let sw = new Rectangle(x,y+h/2,w/2,h/2);
+        let se = new Rectangle(x+w/2,y+h/2,w/2,h/2);
+        
         this.northWest = new QuadTree(nw, this.capacity);
-        let ne = new Rectangle(x+w/2, y-h/2, w/2, h/2);
         this.northEast = new QuadTree(ne, this.capacity);
-        let sw = new Rectangle(x-w/2, y+h/2, w/2, h/2);
         this.southWest = new QuadTree(sw, this.capacity);
-        let se = new Rectangle(x+w/2, y+h/2, w/2, h/2);
         this.southEast = new QuadTree(se, this.capacity);
+        
+        this.divs = [this.northWest,this.northEast,this.southWest,this.southEast];
+        
         this.divided = true;
     }
 
     insert(point) {
-        if(!this.boundary.contains(point)){
-            return;
+        
+        if (!this.boundary.contains(point)){
+            return false;
         }
 
         if (this.points.length < this.capacity) {
             this.points.push(point);
         } else {
-            if(!this.divided){
+            if (!this.divided){
                 this.subdivide();
             }
+
+            if(this.northWest.insert(point)){return true}
+            else if (this.northEast.insert(point)){return true;}
+            else if (this.southWest.insert(point)){return true;}
+            else if (this.southEast.insert(point)){return true;}
+        }
+    }
+
+    query(range,found){
+        if (!found){
+            found = [];
+        }
+        
+        if(!this.boundary.intersects(range)){
+            // empty array
+            return found;
+        } else {
+            for (let p of this.points){
+                if (range.contains(p)) {
+                    found.push(p);
+                }
+            }
+
+            if (this.divided){
+                this.northWest.query(range, found);
+                this.northEast.query(range, found);
+                this.southWest.query(range, found);
+                this.southEast.query(range, found);
+            }
+
+            return found;
+        }
+    }
+
+    check(){
+        if (!this.divided){
+            return;
+        }
+        
+        this.northWest.check();
+        this.northEast.check();
+        this.southWest.check();
+        this.southEast.check();
+
+        if (this.points.length + 
+            this.northWest.points.length +
+            this.northEast.points.length +
+            this.southWest.points.length +
+            this.southEast.points.length <= this.capacity){
+                for(let div of this.divs){
+                    for(let point of div.points){
+                        this.points.push(point);
+                    }
+                }
+                delete this.divs[0-3]
+                this.divided = false;
+            }
+    }
+
+    move(qTree, dt){
+        let count = 0
+
+        for (let p of this.points){
+            p.pmove(dt);
             
-            this.northWest.insert(point);
-            this.northEast.insert(point);
-            this.southWest.insert(point);
-            this.southEast.insert(point);
+            if(!this.boundary.contains(p) && p.check){
+                qTree.insert(p);
+                this.points.splice(count,1);
+            }
+        
+            count ++;
+        }
+
+        if(this.divided){
+            this.northWest.move(qTree, dt);
+            this.northEast.move(qTree, dt);
+            this.southWest.move(qTree, dt);
+            this.southEast.move(qTree, dt);
         }
     }
 
     show(ctx){
-        ctx.strokeStyle = "white";
-        ctx.
-        ctx.strokeRect(this.boundary.x, this.boundary.y, this.boundary.w * 2, this.boundary.h * 2);
-        if(this.divided){
+        ctx.strokeStyle = "#434343";
+        ctx.strokeRect(this.boundary.x, this.boundary.y, this.boundary.w, this.boundary.h);
+        if (this.divided){
             this.northWest.show(ctx);
             this.northEast.show(ctx);
             this.southWest.show(ctx);
-            this.southWest.show(ctx);
+            this.southEast.show(ctx); 
+        }
+        ctx.fillStyle = "white"
+        
+        for (let p of this.points){
+            ctx.beginPath()
+            ctx.arc(p.x, p.y, 2, 0, 2 * Math.PI, false);
+            ctx.closePath()
+            ctx.fill();
         }
     }
 }
